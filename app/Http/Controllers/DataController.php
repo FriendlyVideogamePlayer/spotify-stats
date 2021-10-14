342031da15029c9049adf81d446c41e94a648a82
FriendlyVideogamePlayer <zakariahahah@gmail.com>
2021-10-14 22:22:12 +0100

    Update PHPDoc comments

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 use Session;
 use Illuminate\Support\Facades\Http;
 
 class DataController extends Controller
 {
-    // Refreshes the accessToken that is required to run any other API call and overwrites the current value in the session
-    function refreshDataAccess() {
+
+
+    /**
+     * Refreshes the accessToken that is required to run any other API call and overwrites the current value in the session
+     *
+     */
+    function refreshDataAccess(): void
+    {
         $authorization = base64_encode("".env('SPOTIFY_CLIENT_ID').":".env('SPOTIFY_CLIENT_SECRET'));
 
         $response = Http::asForm()->withHeaders([
@@ -27,9 +35,12 @@ class DataController extends Controller
     /**
      * Gets a users top 50 tracks/artists depending on which route was visited
      * Also checks if a time query string exists and uses it in the api call if it does
-     * Returns a view that displays the top 50 artists/tracks in a specific time period
-     */ 
-    function getTop($type) {
+     *
+     * @param $type
+     * @return View
+     */
+    function getTop($type)
+    {
         if(!Session::has('accessToken')) {
             return redirect('/');
         }
@@ -38,11 +49,11 @@ class DataController extends Controller
         }
 
         $range = isset($_GET['t']) ? $_GET['t'] : "short_term";
-    
+
         $response = Http::withHeaders([
             'Authorization' => 'Bearer '.session('accessToken'),
         ])->get('https://api.spotify.com/v1/me/top/'.$type.'?time_range='.$range.'&limit=50');
-        
+
         $data = $response->json();
 
         if(isset($data['error']['status'])) {
@@ -71,8 +82,14 @@ class DataController extends Controller
         return view('dataDisplay')->with(['items' => $data['items'], 'type' => $type, 'range' => $range]);
     }
 
-    // Gets a users recommended tracks based upon their top artists using the seeds from the above function and returns the recommendations view to display them
-    function getRecommendations() {
+
+    /**
+     * Gets a users recommended tracks based upon their top artists using the seeds from the above function and returns the recommendations view to display them
+     *
+     * @return View|RedirectResponse
+     */
+    function getRecommendations()
+    {
         if(!Session::has('accessToken')) {
             return redirect('/');
         }
@@ -80,7 +97,7 @@ class DataController extends Controller
         $response = Http::withHeaders([
             'Authorization' => 'Bearer '.session('accessToken'),
         ])->get('https://api.spotify.com/v1/recommendations?limit=50&seed_artists='.session('recommendedSeeds'));
-        
+
         $data = $response->json();
 
         if(isset($data['error']['status'])) {
@@ -89,16 +106,21 @@ class DataController extends Controller
                 return $this->getRecommendations();
             }
         }
-        
+
         return view('dataDisplay')->with(['items' => $data['tracks'], 'type' => 'recommendations']);
     }
 
-    // Gets a user's spotify display name and image to display in the navbar
-    function getDisplayInfo() {
+
+    /**
+     * Gets a user's spotify display name and image to display in the navbar
+     *
+     */
+    function getDisplayInfo(): void
+    {
         $response = Http::withHeaders([
             'Authorization' => 'Bearer '.session('accessToken'),
         ])->get('https://api.spotify.com/v1/me');
-        
+
         $data = $response->json();
 
         session(['username' => $data['display_name']]);
@@ -110,12 +132,18 @@ class DataController extends Controller
         }
     }
 
-    // Gets a list of all playlists a user follows or owns, returns the playlist selector view with all the playlists inserted into a select
-    function getPlaylists($offset = null) {
+    /**
+     * Gets a list of all playlists a user follows or owns, returns the playlist selector view with all the playlists inserted into a select
+     *
+     * @param null $offset
+     * @return RedirectResponse|View
+     */
+    function getPlaylists($offset = null)
+    {
         if(!Session::has('accessToken')) {
             return redirect('/');
         }
-        
+
         $offsetVal = ($offset !== null) ? $offset : 0;
         $offset = $offsetVal;
 
@@ -127,7 +155,7 @@ class DataController extends Controller
         $response = Http::withHeaders([
             'Authorization' => 'Bearer '.session('accessToken'),
         ])->get('https://api.spotify.com/v1/me/playlists?limit=50&offset='.$offsetVal);
-        
+
         $data = $response->json();
 
         $offset += 50;
@@ -154,8 +182,15 @@ class DataController extends Controller
         return view('playlists')->with(['items' => session('playlistIds')]);
     }
 
-    // Gets all of the tracks within a specified playlist
-    function getPlaylistTracks($playlistId, $offset = null) {
+    /**
+     * Gets all of the tracks within a specified playlist
+     *
+     * @param $playlistId
+     * @param null $offset
+     * @return RedirectResponse|View
+     */
+    function getPlaylistTracks($playlistId, $offset = null)
+    {
         if(!Session::has('accessToken')) {
             return redirect('/');
         }
@@ -172,7 +207,7 @@ class DataController extends Controller
         $response = Http::withHeaders([
             'Authorization' => 'Bearer '.session('accessToken'),
         ])->get('https://api.spotify.com/v1/playlists/'.$playlistId.'/tracks?fields=items(track(id%2Cname%2Cartists))%2C%20total%2C%20offset%2C%20limit%2C%20next%2C%20previous&limit=100&offset='.$offsetVal);
-        
+
         $data = $response->json();
 
         if(isset($data['error']['status'])) {
@@ -181,7 +216,7 @@ class DataController extends Controller
                 return $this->getPlaylistTracks($playlistId, $offset);
             }
         }
-        
+
         $trackCount = $data['total'];
         $offset += 100;
 
@@ -203,8 +238,15 @@ class DataController extends Controller
         return $this->getTrackFeatures(0, $trackCount);
     }
 
-    // Gets the Spotify "features" of a track such as danceability for all tracks within a playlist
-    function getTrackFeatures($offset = null, $trackCount) {
+    /**
+     * Gets the Spotify "features" of a track such as danceability for all tracks within a playlist
+     *
+     * @param null $offset
+     * @param $trackCount
+     * @return View
+     */
+    function getTrackFeatures($offset = null, $trackCount)
+    {
         $offsetVal = ($offset !== null) ? $offset : 0;
         $offset = $offsetVal;
 
@@ -220,7 +262,7 @@ class DataController extends Controller
         $response = Http::withHeaders([
             'Authorization' => 'Bearer '.session('accessToken'),
         ])->get('https://api.spotify.com/v1/audio-features?ids='.$trackIdList);
-        
+
         $data = $response->json();
 
         if(isset($data['error']['status'])) {
@@ -245,7 +287,7 @@ class DataController extends Controller
 			$duration_s = floor(($duration_ms / 1000) % 60);
 			$formating = '%02u:%02u';
             $duration = sprintf($formating, $duration_m, $duration_s);
-                
+
             Session::push('playlistStats.'.$item['id'].'.danceability', $item['danceability']);
             Session::push('playlistStats.'.$item['id'].'.energy', $item['energy']);
             Session::push('playlistStats.'.$item['id'].'.loudness', $item['loudness']);
@@ -301,15 +343,15 @@ class DataController extends Controller
         $tempoTrack = session('trackIds.'.array_search($maxTempoKey, array_column(session('trackIds'), 1)).'.0');
         $valenceTrack = session('trackIds.'.array_search($maxValenceKey, array_column(session('trackIds'), 1)).'.0');
         $durationTrack = session('trackIds.'.array_search($maxDurationKey, array_column(session('trackIds'), 1)).'.0');
-        
+
         $features = [$maxDanceabilityKey, $maxEnergyKey, $maxLoudnessKey, $maxTempoKey, $maxValenceKey, $maxDurationKey];
         $trackLinks = [];
         foreach($features as $feature) {
             array_push($trackLinks,'https://open.spotify.com/track/'.$feature);
         }
 
-        $trackArray = [['fType' => 'danceability', 'feature' => $danceabilityTrack, 'link' => $trackLinks[0], 'max' => $maxVals[0]],['fType' => 'energy', 'feature'=> $energyTrack, 'link' => $trackLinks[1], 'max' => $maxVals[1]], 
-        ['fType' => 'loudness', 'feature' => $loudnessTrack, 'link' => $trackLinks[2], 'max' => $maxVals[2]], ['fType' => 'tempo', 'feature' => $tempoTrack, 'link' => $trackLinks[3], 'max' => $maxVals[3]], 
+        $trackArray = [['fType' => 'danceability', 'feature' => $danceabilityTrack, 'link' => $trackLinks[0], 'max' => $maxVals[0]],['fType' => 'energy', 'feature'=> $energyTrack, 'link' => $trackLinks[1], 'max' => $maxVals[1]],
+        ['fType' => 'loudness', 'feature' => $loudnessTrack, 'link' => $trackLinks[2], 'max' => $maxVals[2]], ['fType' => 'tempo', 'feature' => $tempoTrack, 'link' => $trackLinks[3], 'max' => $maxVals[3]],
         ['fType' => 'valence', 'feature' => $valenceTrack, 'link' => $trackLinks[4], 'max' => $maxVals[4]], ['fType' => 'duration', 'feature' => $durationTrack, 'link' => $trackLinks[5], 'max' => $maxVals[5]]];
 
 
@@ -317,8 +359,14 @@ class DataController extends Controller
         'valenceVals' => $valenceVals, 'maxVals' => $maxVals]);
     }
 
-    // Gets the current track a user is listening to on spotify
-    function getCurrentTrack() {
+
+    /**
+     * Gets the current track a user is listening to on spotify
+     *
+     * @return RedirectResponse|View
+     */
+    function getCurrentTrack()
+    {
         if(!Session::has('accessToken')) {
             return redirect('/');
         }
@@ -326,7 +374,7 @@ class DataController extends Controller
         $response = Http::withHeaders([
             'Authorization' => 'Bearer '.session('accessToken'),
         ])->get('https://api.spotify.com/v1/me/player/currently-playing');
-        
+
         $data = $response->json();
 
         if(isset($data['error']['status'])) {
@@ -343,7 +391,12 @@ class DataController extends Controller
         return $this->getRecommendationsFromTrack($data['item']);
     }
 
-    // Gets recommended tracks from a specified track
+    /**
+     * Gets recommended tracks from a specified track
+     *
+     * @param $trackInfo
+     * @return RedirectResponse|View
+     */
     function getRecommendationsFromTrack($trackInfo) {
         if(!Session::has('accessToken')) {
             return redirect('/');
@@ -352,7 +405,7 @@ class DataController extends Controller
         $response = Http::withHeaders([
             'Authorization' => 'Bearer '.session('accessToken'),
         ])->get('https://api.spotify.com/v1/recommendations?limit=20&seed_tracks='.$trackInfo['id']);
-        
+
         $data = $response->json();
 
         if(isset($data['error']['status'])) {
@@ -361,7 +414,7 @@ class DataController extends Controller
                 return $this->getRecommendationsFromTrack();
             }
         }
-    
+
         return view('dataDisplay')->with(['items' => $data['tracks'], 'type' => 'currentTrack', 'currentTrackInfo' => $trackInfo]);
     }
 
